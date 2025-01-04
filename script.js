39e134f88d5731bed3b60ec1fca3b191c25fb1a0
Martin Kilmartin <martinkilmartin@gmail.com>
2025-01-04 12:22:24 -0500

    Try to fix script

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
 const emojiList = document.getElementById('emoji-list');
 
+// Fetch emoji data and render it
 fetch('openmoji.json')
   .then((res) => res.json())
   .then((data) => {
-    const emojis = data.map((emoji) => ({
-      char: emoji.hexcode, // Use hexcode for SVG
-      name: emoji.annotation,
-      category: emoji.group,
-    }));
-    renderEmojis(emojis);
-  });
+    renderEmojis(data);
+  })
+  .catch((err) => console.error('Error loading emoji data:', err));
 
+// Function to render emojis
 function renderEmojis(emojis) {
   emojiList.innerHTML = emojis
     .map(
       (emoji) => `
       <div class="emoji" role="button" tabindex="0" title="${emoji.annotation}">
         <img 
-          src="https://cdn.jsdelivr.net/npm/@openmoji/svg@14.0.0/color/${formatHexcode(
+          src="https://cdn.jsdelivr.net/npm/@openmoji@14.0.0/color/svg/${
             emoji.hexcode
-          )}.svg" 
+          }.svg" 
           alt="${emoji.annotation}" 
-          loading="lazy"
+          loading="lazy" 
           style="width: 48px; height: 48px;"
         >
         <small>${emoji.annotation}</small>
+        <p>${emoji.tags ? emoji.tags : ''}</p>
       </div>
     `
     )
     .join('');
-}
-
-function formatHexcode(hexcode) {
-  return hexcode.toUpperCase().replace(/ /g, '-');
 }
